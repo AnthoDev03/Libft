b40ae9add6b83d46c40840a1b97261f68a44ac7d
anthony <anthonydev42@gmail.com>
2023-04-12 12:24:03 +0200

    some fixe

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,15 +1,18 @@
 #include "libft.h"
 
-char    *ft_strchr(char *string, int character)
+char *ft_strchr(const char *string, int character)
 {
     int i;
 
     i = 0;
-    while(string[i] || character == '\0')
+    while(string[i] != '\0')
     {
         if (string[i] == character)
-            return &string[i];
+            return (char *)&string[i];
         i++;
     }
+    if (character == '\0')
+        return (char *)&string[i];
     return 0;
 }
+
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,32 +2,30 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
-	char	*p_src;
+	size_t i;
+	size_t j;
 
 	if (size == 0)
-    {
-        return 0;
-    }
+		return 0;
+
 	j = 0;
 	i = 0;
-	p_src = (char *)src;
 	while (i < size)
 	{
 		if (dst[i] == 0)
-			break ;
+			break;
 		if (i == size - 1 && dst[i])
 			return (size + ft_strlen(src));
 		++i;
 	}
 	i = ft_strlen(dst);
-	while (i + j < size - 1 && p_src[j] && 0 < size)
+	while (i + j < size - 1 && src[j])
 	{
-		dst[i + j] = p_src[j];
+		dst[i + j] = src[j];
 		++j;
 	}
-	dst[i + j] = 0;
+	if (i + j < size)
+		dst[i + j] = 0;
 	if (size < i)
 		return (size + ft_strlen(src));
 	return (i + ft_strlen(src));
