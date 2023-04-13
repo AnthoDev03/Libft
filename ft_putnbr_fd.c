9b912563140914d6994c17bea956920eba440599
anthony <anthonydev42@gmail.com>
2023-04-13 10:40:56 +0200

    part 1 and 2

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+#include <unistd.h>
+
+void ft_putnbr_fd(int n, int fd)
+{
+    char c;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        write(fd, "-", 1);
+        num = (unsigned int)(-n);
+    }
+    else
+    {
+        num = (unsigned int)n;
+    }
+
+    if (num >= 10)
+    {
+        ft_putnbr_fd(num / 10, fd);
+    }
+
+    c = (num % 10) + '0';
+    write(fd, &c, 1);
+}
+
